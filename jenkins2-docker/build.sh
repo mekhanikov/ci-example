a1819b6f0565eff233f9a943f8bb85ce886f01f9
Evgeniy Mekhanikov <emekhanikov@thumbtack.net>
2016-10-20 16:43:50 +0600

    initiali

--- jenkins2-docker/build.sh
+++ jenkins2-docker/build.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 docker build -t jenkins-data -f Dockerfile-data .
 docker build -t jenkins2 .
+
+
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
+git clone ssh://git@localhost:10022/root/mod-a.git
+
+git clone ssh://git@ciinfraexaple_gitlab_1:22/root/mod-a.git
